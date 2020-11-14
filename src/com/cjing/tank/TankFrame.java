4331415a6286700ddcadecbc29358233ccf6b3f1
cjing9017 <cjing9017@163.com>
2020-11-14 21:22:01 +0800

    解决屏幕闪烁问题

--- src/com/cjing/tank/TankFrame.java
+++ src/com/cjing/tank/TankFrame.java
@@ -11,8 +11,11 @@ public class TankFrame extends Frame {
     private Tank myTank = new Tank(200, 200, Dir.DOWN);
     private Bullet bullet = new Bullet(300, 300, Dir.DOWN);
 
+    private static final int GAME_WIDTH = 800;
+    private static final int GAME_HEIGHT = 800;
+
     public TankFrame() throws HeadlessException {
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
@@ -25,6 +28,21 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+    }
+
+    private Image offScreenImage = null;
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.black);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     @Override
